2d2bce9b3b15708660aa47361f00e6fb6bcc6eea
awooken <85866723+awooken@users.noreply.github.com>
2021-06-14 00:52:22 -0700

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,56 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+user_choice = input("Choose either 'rock' 'paper' or 'scissors'. \n")
+
+choices = [rock, paper, scissors]
+bot_choice = random.choice(choices)
+
+if user_choice == "rock":
+    print(rock)
+    if bot_choice == rock:
+        print(f"Bot chose: \n {bot_choice}\n It's a draw!")
+    elif bot_choice == paper:
+        print(f"Bot chose: \n {bot_choice}\n You lost!")
+    if bot_choice == scissors:
+        print(f"Bot chose: \n {bot_choice}\n You won!")
+elif user_choice == paper:
+    print(paper)
+    if bot_choice == rock:
+        print(f"Bot chose: \n {bot_choice}\n You won!")
+    elif bot_choice == paper:
+        print(f"Bot chose: \n {bot_choice}\n It's a draw!")
+    if bot_choice == scissors:
+        print(f"Bot chose: \n {bot_choice}\n You lost!")
+elif user_choice == scissors:
+    print(scissors)
+    if bot_choice == rock:
+        print(f"Bot chose: \n {bot_choice}\n You lost!!")
+    elif bot_choice == paper:
+        print(f"Bot chose: \n {bot_choice}\n You won!")
+    if bot_choice == scissors:
+        print(f"Bot chose: \n {bot_choice}\n It's a draw!")
