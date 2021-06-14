abaddbb3ad608022322542aa30454cd50471b465
awooken <85866723+awooken@users.noreply.github.com>
2021-06-14 01:08:59 -0700

    Update rps.py

--- rps.py
+++ rps.py
@@ -39,7 +39,7 @@ if user_choice == "rock":
         print(f"Bot chose: \n {bot_choice}\n You lost!")
     else:
         print(f"Bot chose: \n {bot_choice}\n You won!")
-elif user_choice == paper:
+elif user_choice == "paper":
     print(paper)
     if bot_choice == rock:
         print(f"Bot chose: \n {bot_choice}\n You won!")
@@ -47,7 +47,7 @@ elif user_choice == paper:
         print(f"Bot chose: \n {bot_choice}\n It's a draw!")
     else:
         print(f"Bot chose: \n {bot_choice}\n You lost!")
-elif user_choice == scissors:
+elif user_choice == "scissors":
     print(scissors)
     if bot_choice == rock:
         print(f"Bot chose: \n {bot_choice}\n You lost!!")
